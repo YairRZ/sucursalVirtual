ab3ae4591b4896ec0e7eae74a7692c2d409d8a8b
peredo98 <36453392+peredo98@users.noreply.github.com>
2018-04-15 00:17:22 -0500

    Correcion registro y confirmacion terminada

---
+++ src/pages/confirmacion/confirmacion.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
+
+import { User } from '../../providers/providers';
+import { MainPage } from '../pages';
+
+/**
+ * Generated class for the ConfirmacionPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-confirmacion',
+  templateUrl: 'confirmacion.html',
+})
+export class ConfirmacionPage {
+account: { name: string, email: string, password: string } = {
+    name: 'Test Human',
+    email: 'test@example.com',
+    password: 'test'
+  };
+
+  // Our translated text strings
+  private signupErrorString: string;
+
+  constructor(public navCtrl: NavController,
+    public user: User,
+    public toastCtrl: ToastController,
+    public translateService: TranslateService) {
+
+    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
+      this.signupErrorString = value;
+    })
+  }
+   doSignup() {
+    // Attempt to login in through our User service
+    this.user.signup(this.account).subscribe((resp) => {
+      this.navCtrl.push(MainPage);
+    }, (err) => {
+
+      this.navCtrl.push(MainPage);
+
+      // Unable to sign up
+      let toast = this.toastCtrl.create({
+        message: this.signupErrorString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+   	 });
+	}
+}
+
+
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -14,40 +14,17 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
-  };
-
-  // Our translated text strings
-  private signupErrorString: string;
-
-  constructor(public navCtrl: NavController,
-    public user: User,
-    public toastCtrl: ToastController,
-    public translateService: TranslateService) {
-
-    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
-      this.signupErrorString = value;
-    })
+  
+  constructor(public navCtrl: NavController) {
   }
 
-  doSignup() {
-    // Attempt to login in through our User service
-    this.user.signup(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
 
-      this.navCtrl.push(MainPage);
 
-      // Unable to sign up
-      let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ConfirmacionPage');
   }
+  confirmacion() {
+      this.navCtrl.push('ConfirmacionPage');
+    }
+ 
 }
