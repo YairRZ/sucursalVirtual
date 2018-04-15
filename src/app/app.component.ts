e82a288542876aa26fdf20b709932abab8b0fff6
peredo98 <36453392+peredo98@users.noreply.github.com>
2018-04-15 11:46:03 -0500

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { FirstRunPage } from '../pages/pages';
 import { Settings } from '../providers/providers';
 
 @Component({
-  template: `<ion-menu [content]="content">
+  template: `<ion-menu [content]="content" [swipeEnabled]="false">
     <ion-header>
       <ion-toolbar>
         <ion-title>Pages</ion-title>
